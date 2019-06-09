329b1cb342765de3464c69eea5dfb9d7ec64b042
Ziaul-Haque MAHOMED <zmahomed@c4r1s5.wethinkcode.co.za>
2019-06-09 04:25:34 +0200

    This

--- floor_map.sh
+++ floor_map.sh
@@ -8,11 +8,22 @@ row=1
 
 while :
 do
-	if [[ ${seat} == 1 && ${row} -lt 11 ]]
+	if [ ${row} == 18 ]
+		then printf "\n"
+	fi
+	if [[ ${seat} == 1 && ${row} -lt 10 ]]
 		then printf ${row}"  "
 	fi
-	if [[ ${seat} == 1 && ${row} -gt 11 ]]
+	if [[ ${seat} == 1 && ${row} -gt 9 && ${row} -lt 18 ]]
 		then printf ${row}" "
+	fi
+	if [ ${seat} == 6 ]
+		then printf "  "
+	fi
+	if [[ ${row} == 17 && ${seat} == 1 ]]
+	then 
+			printf "            " 
+			seat=6
 	fi
 	if gtimeout 0.01 ping $1r${row}s${seat} -c 1 | grep -q "64"
 		then printf "⚪"
@@ -24,7 +35,7 @@ do
 			((row++))
 			seat=0
 	fi
-	if [[ ${row} -lt 17 && ${seat} -gt 10 ]]
+	if [[ ${row} -lt 18 && ${seat} -gt 10 ]]
 	then
 			printf "\n"
 			((row++))
@@ -32,6 +43,5 @@ do
 	fi
 	((seat++))
 	((counter++))
-	sleep 1
 done
 printf "\n"
