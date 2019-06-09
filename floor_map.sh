a0156e97c83f881318c819b61e6bac54c13ab628
Marnus BOTES <mbotes@c4r1s3.wethinkcode.co.za>
2019-06-09 05:08:50 +0200

    BOB

--- floor_map.sh
+++ floor_map.sh
@@ -6,16 +6,27 @@ counter=0
 seat=1
 row=1
 
-while :
+while [ $row -le 17 ]
 do
-	if [[ ${seat} == 1 && ${row} -lt 11 ]]
+	if [ ${row} == 18 ]
+		then printf "\n"
+	fi
+	if [[ ${seat} == 1 && ${row} -lt 10 ]]
 		then printf ${row}"  "
 	fi
-	if [[ ${seat} == 1 && ${row} -gt 11 ]]
+	if [[ ${seat} == 1 && ${row} -gt 9 && ${row} -lt 18 ]]
 		then printf ${row}" "
 	fi
+	if [ ${seat} == 6 ]
+		then printf "  "
+	fi
+	if [[ ${row} == 17 && ${seat} == 1 ]]
+	then 
+			printf "            " 
+			seat=6
+	fi
 	if gtimeout 0.01 ping $1r${row}s${seat} -c 1 | grep -q "64"
-		then printf "⚪"
+		then printf "💻"
 		else printf "❌"
 	fi
 	if [[ ${row} -lt 5 && ${seat} -gt 4 ]]
@@ -24,7 +35,7 @@ do
 			((row++))
 			seat=0
 	fi
-	if [[ ${row} -lt 17 && ${seat} -gt 10 ]]
+	if [[ ${row} -le 17 && ${seat} -gt 10 ]]
 	then
 			printf "\n"
 			((row++))
@@ -32,6 +43,27 @@ do
 	fi
 	((seat++))
 	((counter++))
-	sleep 1
+done
+printf "\n\n"
+seat=5
+while [ $seat -ge 1 ]
+do
+	row=18
+	printf "		"
+	while [ $row -le 25 ]
+	do
+	if [[ $row == 22 && $seat == 5 || $row == 25  && $seat == 5 ]]
+	then
+		printf "  "
+	else 
+	if gtimeout 0.01 ping $1r${row}s${seat} -c 1 | grep -q "64"
+		then printf "💻"
+		else printf "❌"
+	fi
+fi
+	printf '   '
+	((row++))
 done
 printf "\n"
+((seat--))
+done
