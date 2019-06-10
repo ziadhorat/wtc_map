543eca81e438a77149a85a91a2b026d7f5061c70
Marnus BOTES <mbotes@c4r21s4.wethinkcode.co.za>
2019-06-10 08:32:06 +0200

    INSTALLER SH

---
+++ installer.sh
@@ -0,0 +1,4 @@
+echo "Welcome to WTC_MAP INSTAL"\
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
+ brew install coreutils
+echo "Finish INSTALLING"
